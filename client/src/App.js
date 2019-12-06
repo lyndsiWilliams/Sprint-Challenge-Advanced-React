685ec7ffe5ef759713b44fa80f2a166ffa4ddc8b
lyndsiWilliams <kcatgirl@gmail.com>
2019-12-06 11:12:17 -0600

    Implemented custom hooks useLocalStorage and useDarkMode with 'light mode' toggle button

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './App.css';
 
 import { Players } from './components/Players';
+import { useDarkMode } from './hooks/useDarkMode';
 
 function App() {
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
+
   return (
     <div className="App">
+      <button onClick={toggleMode}>Light mode</button>
       <Players />
     </div>
   );
