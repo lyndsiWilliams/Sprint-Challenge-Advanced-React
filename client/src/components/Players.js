8154243f325d94377f510623bbdb19b50862ffc1
lyndsiWilliams <kcatgirl@gmail.com>
2019-12-06 10:25:17 -0600

    Structured Players class component

---
+++ client/src/components/Players.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import axios from 'axios';
+
+class Players extends React.Component {
+  state = { player: null };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/players")
+    .then(response => {
+      console.log(response.data);
+      this.setState({ player: response.data });
+    })
+    .catch(error => {
+      console.log("No player data returned", error)
+    })
+  }
+
+  render() {
+    const { player } = this.state;
+
+    if (user === null) {
+      return <div>Loading...</div>
+    }
+
+    return (
+      <PlayerCard player={player} />
+    );
+  }
+}
+
+export default Players;
