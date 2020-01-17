a0599a5133996c1692609486364e3cdc006fb06e
lyndsiWilliams <kcatgirl@gmail.com>
2020-01-17 10:56:25 -0600

    useLocalStorage and useDarkMode hooks implemented and functional, API data pulled and displaying successfully

---
+++ client/src/components/Players.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import axios from 'axios';
+
+import { PlayerCard } from './PlayerCard';
+
+export class Players extends React.Component {
+  state = { players: [] };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/players")
+    .then(response => {
+      console.log(response.data);
+      this.setState({ players: response.data });
+    })
+    .catch(error => {
+      console.log("No player data returned", error)
+    })
+  }
+
+  render() {
+    const { players } = this.state;
+
+    if (players === []) {
+      return <div>Loading...</div>
+    }
+
+    return (
+      players.map((players, index) => {
+        return (
+          <PlayerCard key={index} players={players} />
+        )
+      })
+    );
+  }
+}
